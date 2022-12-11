2bc4676832ef56e2c7499984230ec43009491d34
Anthony <acmiyaguchi@gmail.com>
2022-12-10 17:45:57 -0800

    Figure out how to make model work correctly

--- birdcall_distribution/model.py
+++ birdcall_distribution/model.py
@@ -2,6 +2,7 @@ import numpy as np
 import pymc as pm
 
 from birdcall_distribution.data import prepare_scaled_data
+from birdcall_distribution.geo import get_modis_land_cover_name
 
 
 def _scaled_data_old(prep_df):
@@ -27,9 +28,15 @@ def _scaled_data(prep_df):
 
 def _coords(prep_df, scaled_data_df):
     species_cat = prep_df.primary_label.astype("category")
-    n_features = scaled_data_df.shape[1]
+    snake_case = lambda s: s.replace(" ", "_").replace("/", "_").lower()
+    norm = (
+        lambda c: f"{c}_{snake_case(get_modis_land_cover_name(c))}"
+        if "land_cover" in c
+        else c
+    )
+    features = [norm(c) for c in scaled_data_df.columns.values]
     coords = dict(
-        features_idx=scaled_data_df.columns,
+        features_idx=features,
         species_idx=species_cat.cat.categories,
         adj_idx=sorted(prep_df.index.unique()),
         obs_idx=np.arange(prep_df.shape[0]),
@@ -108,15 +115,15 @@ def make_pooled_intercept_car_model(prep_df, W, *args, **kwargs):
 
     with pm.Model(coords=_coords(prep_df, scaled_data_df)) as model:
         adj_idx = pm.ConstantData(
-            "adj_idx", prep_df.index.values.astype(int).astype(int), dims="obs_idx"
+            "adj_idx", prep_df.index.values.astype(int), dims="obs_idx"
         )
 
         alpha = pm.Beta("alpha", 5, 1)
-        sigma_phi = pm.Uniform("sigma_phi", 0, 20)
+        tau_phi = pm.Gamma("tau_phi", 1e-3, 1e-3)
         phi = pm.CAR(
             "phi",
             mu=np.zeros(W.shape[0]),
-            tau=1 / sigma_phi,
+            tau=tau_phi,
             alpha=alpha,
             W=W,
             dims="adj_idx",
@@ -128,7 +135,7 @@ def make_pooled_intercept_car_model(prep_df, W, *args, **kwargs):
         pm.Poisson(
             "y",
             mu=mu,
-            observed=np.ma.masked_invalid(prep_df.y.values).filled(0),
+            observed=np.ma.masked_invalid(prep_df.y.values).filled(1e-6),
             dims="obs_idx",
         )
     return model
@@ -335,12 +342,10 @@ def make_pooled_intercept_pooled_covariate_car_model(prep_df, W, *args, **kwargs
         adj_idx = pm.ConstantData(
             "adj_idx", prep_df.index.values.astype(int), dims="obs_idx"
         )
-        X = pm.ConstantData(
-            "X", scaled_data_df.values, dims=("obs_idx", "features_idx")
-        )
+        X = pm.ConstantData("X", scaled_data_df, dims=("obs_idx", "features_idx"))
 
         alpha = pm.Beta("alpha", 5, 1)
-        tau_phi = pm.Gamma("tau_phi", 1e-3, 1e-3)
+        tau_phi = pm.Gamma("tau_phi", 1, 1)
         phi = pm.CAR(
             "phi",
             mu=np.zeros(W.shape[0]),
@@ -350,17 +355,25 @@ def make_pooled_intercept_pooled_covariate_car_model(prep_df, W, *args, **kwargs
             dims="adj_idx",
         )
 
+        # sum to zero constraint?
+        # https://discourse.pymc.io/t/writing-tests-for-the-log-probability-of-the-sum-to-zero-icar-prior-via-pm-potential/10144/3
+        # logp_func = pm.Normal.dist(mu=0.0, sigma=np.sqrt(0.001))
+        # pm.Potential("zero_sum", pm.logp(logp_func, pm.math.sum(phi)))
+
         intercept = pm.Normal("intercept", mu=0, tau=1e-3)
         betas = pm.Normal("betas", mu=0, tau=1e-3, dims="features_idx")
+        # print(pm.math.dot(X, betas).eval().shape)
+        # print(phi[adj_idx].eval().shape)
         mu = pm.Deterministic(
             "mu",
             pm.math.exp(intercept + pm.math.sum(X * betas, axis=1) + phi[adj_idx]),
             dims="obs_idx",
         )
+        # print(mu.eval().shape)
         pm.Poisson(
             "y",
             mu=mu,
-            observed=np.ma.masked_invalid(prep_df.y.values).filled(0),
+            observed=np.ma.masked_invalid(prep_df.y.values).filled(1e-3),
             dims="obs_idx",
         )
     return model
--- birdcall_distribution/plot.py
+++ birdcall_distribution/plot.py
@@ -147,7 +147,7 @@ def plot_ppc_species(
     pred_df["pred"] = ppc.posterior_predictive.y.values.reshape(-1, shape[0]).mean(
         axis=0
     )
-    pred_df["log_pred"] = np.log(pred_df.pred)
+    pred_df["log_pred"] = np.log(pred_df.pred + 1)
     sub_df = pred_df[prep_df.primary_label == species]
 
     if show_hist:
@@ -243,7 +243,7 @@ def plot_ppc_species_subplot(
 
     df = df.copy()
     df["pred"] = ppc.posterior_predictive.y.values.reshape(-1, df.shape[0]).mean(axis=0)
-    df["log_pred"] = np.log(df.pred)
+    df["log_pred"] = np.log(df.pred + 1)
 
     fig, axs = plt.subplots(
         *shape, figsize=(9, 7), subplot_kw={"projection": ccrs.PlateCarree()}
